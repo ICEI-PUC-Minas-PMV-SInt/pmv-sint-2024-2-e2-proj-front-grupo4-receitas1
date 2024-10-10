13f13fdc6b9045af9bff91cb0923a0f2117cb216
Tatiana Haveroth Barbosa <haverothtatiana@gmail.com>
2024-10-10 14:22:13 -0300

    Atualizando app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import CafedaManha from './Components/pages/CafedaManha';
 import Almoço from './Components/pages/Almoço';
 import Jantar from './Components/pages/Jantar';
