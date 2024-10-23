23bb0cf069135c92cfb379078057b6a00d770558
Rodrigo da Costa Souza <rodrigo.souza.1539492@sga.pucminas.br>
2024-10-23 10:10:35 -0300

    Criação dos Comentarios - ComentarioAcoes

---
+++ .history/src/Components/ComentarioAcoes_20241023100715.js
@@ -0,0 +1,42 @@
+/** @format */
+
+import React from 'react';
+import styles from './ComentarioAcoes.module.css';
+
+const ComentarioAcoes = ({
+	toggleRespostasVisiveis,
+	removerComentario,
+	respostasVisiveis,
+	qtdeRespostas,
+}) => {
+	return (
+		<div className={styles.comentarioAcoes}>
+			<button
+				className={styles.comentarioRemoverBtn}
+				onClick={removerComentario}
+			>
+				Excluir
+			</button>
+			<button
+				className='comentario-responder-btn'
+				onClick={toggleRespostasVisiveis}
+			>
+				Responder
+			</button>
+			<div className='container-verMaisRespostas-ou-esconderRespostas'>
+				{qtdeRespostas > 0 && (
+					<button
+						className='comentario-ver-mais-btn'
+						onClick={toggleRespostasVisiveis}
+					>
+						{respostasVisiveis
+							? `Esconder respostas`
+							: `Ver mais ${qtdeRespostas} respostas`}
+					</button>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default ComentarioAcoes;
