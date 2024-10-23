0e20a44b753a93a54edd281afd2c457bb9edc099
Rodrigo da Costa Souza <rodrigo.souza.1539492@sga.pucminas.br>
2024-10-23 10:30:11 -0300

    Criação dos Comentarios - RespostasLista

---
+++ .history/src/Components/RespostasLista_20241023102535.js
@@ -0,0 +1,23 @@
+/** @format */
+
+import React from 'react';
+import styles from './RespostasLista.module.css';
+import EnviarRespostaButton from './EnviarRespostaButton';
+
+const RespostasLista = ({ respostas }) => {
+	return (
+		<div className='respostas-lista'>
+			{respostas.map(resposta => (
+				<div key={resposta.id} className='resposta-item'>
+					<img src={resposta.fotoUsuario} alt={resposta.usuario} />
+					<div className='resposta-content'>
+						<span className='resposta-usuario'>{resposta.usuario}</span>
+						<p className='resposta-texto'>{resposta.texto}</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default RespostasLista;
