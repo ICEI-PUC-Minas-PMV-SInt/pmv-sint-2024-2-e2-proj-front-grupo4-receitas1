b39ba5fa6094e551a1c9d619001abbc7de11bb99
Rodrigo da Costa Souza <rodrigo.souza.1539492@sga.pucminas.br>
2024-10-23 09:29:10 -0300

    adicionando UsuarioPostador

---
+++ .history/src/Components/UsuarioPostador_20241023092744.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from 'react';
+import styles from './UsuarioPostador.module.css';
+
+const UsuarioPostador = ({ fotoUsuarioPostador, usuarioPostador }) => {
+	return (
+		<div className={styles.containerUsuarioPostador}>
+			<img
+				className='fotoUsuarioPostador'
+				src={fotoUsuarioPostador}
+				alt={`Foto de ${usuarioPostador}`}
+			/>
+			<p>Por {usuarioPostador}</p>
+		</div>
+	);
+};
+
+export default UsuarioPostador;
