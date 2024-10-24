617aee19d257a2ecb46dce52d203d1f5fcbc0d1f
Rodrigo da Costa Souza <rodrigo.souza.1539492@sga.pucminas.br>
2024-10-24 08:03:43 -0300

    corrigindo imagens esticadas

---
+++ .history/src/Components/ReceitaCard_20241024075756.js
@@ -0,0 +1,48 @@
+/** @format */
+
+import React, { useState } from 'react';
+import UsuarioPostador from './UsuarioPostador.js';
+import { CommentIcon } from './CommentIcon.js';
+import ModalComentarios from './ModalComentarios.js'; // Importe o Modal
+import styles from './ReceitaCard.module.css';
+
+const ReceitaCard = ({
+	fotoReceita,
+	fotoUsuarioPostador,
+	usuarioPostador,
+	nome,
+	id,
+}) => {
+	const [modalAberto, setModalAberto] = useState(false);
+
+	const abrirModal = () => {
+		setModalAberto(true);
+	};
+
+	const fecharModal = () => {
+		setModalAberto(false);
+	};
+
+	return (
+		<div className={styles.containerReceitaCard}>
+			<img
+				className={styles.fotoReceita}
+				src={fotoReceita}
+				alt={`Receita de ${nome}`}
+			/>
+
+			<div className={styles.containerUsuarioEComentarios}>
+				<UsuarioPostador
+					fotoUsuarioPostador={fotoUsuarioPostador}
+					usuarioPostador={usuarioPostador}
+				/>
+				<CommentIcon onClick={abrirModal} /> {/* Abre o modal */}
+			</div>
+
+			{/* Renderiza o modal de comentários, se estiver aberto */}
+			{modalAberto && <ModalComentarios receitaId={id} onClose={fecharModal} />}
+		</div>
+	);
+};
+
+export default ReceitaCard;
